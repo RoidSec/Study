7576b62bf86ec4a98f7f52d9b2c9c024aa78abb8
ChillinIX <70252163+ChillinIX@users.noreply.github.com>
2021-04-20 21:21:03 +1000

    Adding task notes

---
+++ Week 1/Labs/Task 6.py
@@ -0,0 +1,10 @@
+"""
+user input of the date
+showing the date in day/month/year Format
+"""
+
+day = input("What day is it: ")
+month = input("What Month is it: ")
+year = input("What year is it: ")
+
+print (day +"/"+ month +'/'+ year)
