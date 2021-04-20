6e59be7914ff58859b52edb52b4e4cc024c8a9d2
ChillinIX <70252163+ChillinIX@users.noreply.github.com>
2021-04-20 20:44:23 +1000

    Create Task 1 - Swapping Variables.py

---
+++ Week 1/In-Class Tasks/Task 1 - Swapping Variables.py
@@ -0,0 +1,15 @@
+"""
+python program to swap two variables
+"""
+
+x = ('256')
+y = ('4000')
+
+# create a temp variable and swap
+
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('the value of y after swapping: {}'.format(y))
