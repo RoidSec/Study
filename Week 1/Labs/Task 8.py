59d495c744bb1d712d6821f66406331a03b937cc
ChillinIX <70252163+ChillinIX@users.noreply.github.com>
2021-04-20 21:25:46 +1000

    Adding task notes

---
+++ Week 1/Labs/Task 8.py
@@ -0,0 +1,10 @@
+"""
+Research how Python can generate random numbers. Make a simple program that makes 7 random
+numbers – 6 of them are “winning numbers” and 1 is the powerball. Output these to the screen in this format:
+
+	I think the lottery’s winning numbers are [1 2 3 4 5 6] and the powerball should be [7].
+"""
+
+import random
+randomNumbers = random.sample(range(1, 99), 7)
+print("I think the lottery’s winning numbers are:", randomNumbers[0:6], "and the powerball is ["+str(randomNumbers[6])+ "]")
