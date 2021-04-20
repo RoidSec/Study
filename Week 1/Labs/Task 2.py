a252d2f5ecf0c825c0df7bd8bc7e414d1a890f65
ChillinIX <70252163+ChillinIX@users.noreply.github.com>
2021-04-20 20:35:17 +1000

    Adding task notes

--- Week 1/Labs/Task 2.py
+++ Week 1/Labs/Task 2.py
@@ -1,3 +1,24 @@
+"""
+This is an incomplete program that will ask you provide three numbers and add them together:
+
+The programmer forgot to complete their work – they left ???? in those sections. Can you complete
+the program for them?
+
+#This program adds three numbers
+print("please enter three numbers.")
+num1 = int(input("First number:"))
+num2 = int(input(??????????))
+num3 = int(????????????)
+
+What happens if you remove the int( and the last ) from all three input lines? Why? e.g. num1 = int(input("First number: "))  num1 = input("First number: ")
+
+Keep pressing Ctrl-Z to undo your code changes and put the int() back.
+
+Can you change the program to subtract three numbers instead? How about multiply?
+(We haven’t discussed this yet – try Googling for “Python how do I multiply numbers”)
+"""
+
+#This program adds three numbers
 print("please enter three numbers.")
 num1 = int(input("First number:"))
 num2 = int(input("Second number:"))
