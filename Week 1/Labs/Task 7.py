8540569b01b786776f6f52d7e8beadc35fe83dcb
ChillinIX <70252163+ChillinIX@users.noreply.github.com>
2021-04-20 21:24:38 +1000

    Adding task notes

---
+++ Week 1/Labs/Task 7.py
@@ -0,0 +1,41 @@
+"""
+Display a form showing the below information - 
+
+Current date and time using the str method of datetime object:
+[str date and time]
+
+Current Date and Time using instance attributes:
+Year:
+Month:
+Day:
+Hour:
+Minute:
+Second:
+Microsecond:
+
+Current date and time using strftime:
+[strftime date and time]
+
+Current date and time using isoformat
+[iso format date and time]
+"""
+
+import datetime
+now = datetime.datetime.now()
+print("Current date and time using the str method of datetime object:")
+print(now,"\n")
+
+print("Current Date and Time using instance attributes:")
+print("Year:",now.year)
+print("Month:",now.month)
+print("Day:",now.day)
+print("Hour:",now.hour)
+print("Minute:",now.minute)
+print("Second:",now.second)
+print("Microsecond:",now.microsecond,"\n")
+
+print("Current date and time using strftime:")
+print(now.strftime("%c"), "\n")
+
+print("Current date and time using isoformat")
+print (now.isoformat())
