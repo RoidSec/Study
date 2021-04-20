33a4e981f3b49d8e822a351a453f95d2b436da81
ChillinIX <70252163+ChillinIX@users.noreply.github.com>
2021-04-20 20:19:12 +1000

    Create Task 2.py

---
+++ Week 1/Labs/Task 2.py
@@ -0,0 +1,6 @@
+print("please enter three numbers.")
+num1 = int(input("First number:"))
+num2 = int(input("Second number:"))
+num3 = int(input("Third number:"))
+
+print("The sum of these numbers is: ", num1 * num2 - num3)
