7cc241011afe849fa4a98e9dedb3598d0427d515
ChillinIX <70252163+ChillinIX@users.noreply.github.com>
2021-04-20 20:32:09 +1000

    Adding task notes

--- Week 1/Labs/Task 3.py
+++ Week 1/Labs/Task 3.py
@@ -1,3 +1,10 @@
+"""
+Make a program from scratch. Have the program ask for your name and your age, then output them.
+
+Add a new feature: the program needs to tell you how old you’ll be next year – so add one year to
+your age.
+"""
+
 name = input("what is your Name?")
 age = int(input("what is your Age?"))
 
