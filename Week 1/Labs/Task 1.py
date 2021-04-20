f82c50fe36577af73e0e7d4eb7eef074a95ca76a
ChillinIX <70252163+ChillinIX@users.noreply.github.com>
2021-04-20 20:18:40 +1000

    Create Task 1.py

---
+++ Week 1/Labs/Task 1.py
@@ -0,0 +1,5 @@
+#This program outputs Australia Day to the screen
+australiaDayDate = "26 January"
+print ("australia day is on " + australiaDayDate)
+age = "116"
+print ("The oldest person alive is \tEmma Morano,she is\n\t \t \t \t \t \t " + age)
