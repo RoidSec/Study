88709af33a2dce1aee70d891fe5b0d351093e034
ChillinIX <Colin.j.prater@outlook.com>
2021-04-21 18:39:42 +1000

    Updates

--- Week 1/Labs/Task 8.py
+++ Week 1/Labs/Task 8.py
@@ -2,7 +2,7 @@
 Research how Python can generate random numbers. Make a simple program that makes 7 random
 numbers – 6 of them are “winning numbers” and 1 is the powerball. Output these to the screen in this format:
 
-	I think the lottery’s winning numbers are [1 2 3 4 5 6] and the powerball should be [7].
+	I think the lottery’s winning numbers are [1 2 3 4 5 6] and the powerball should be [7]. 
 """
 
 import random
