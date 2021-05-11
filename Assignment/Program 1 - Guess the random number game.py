a6b650975eaf02c4fa7355ca74d0e66ede96f1c4
ChillinIX <Colin.j.prater@outlook.com>
2021-05-12 01:11:24 +1000

    Updates for completion

--- Assignment/Program 1 - Guess the random number game.py
+++ Assignment/Program 1 - Guess the random number game.py
@@ -5,25 +5,29 @@
 #Guess the random number game
 #One player vs. the computer
 
+# Imports and establishing the variable parameters for the guessing game
 import random
 guessMin = 1
 guessMax = 6
 
-#ask the user for their name and their guess
+# Input for the user's name and guess, establishing to the user that they can only use numbers 1 - 6
 name = input("What is your name? ")
-print ("Hi " + name)
-print("Please enter a number between: ", guessMin, " and ", guessMax)
+print ("Hi "+name)
+print("Please enter a number between:",guessMin,"and",guessMax)
 
+# while loop for the user's guess so that if they guess anything other than 1-6 it will give them an error and ask them to enter a valid guess
 while True:
     guessUser = int(input("What is your guess? "))
     if (int(guessUser)<guessMin or int(guessUser)>guessMax):
-        print ("Invalid input. Try again.")
+        print ("Invalid input. Please enter a number between",guessMin,"and",guessMax)
     else:
         break
 
-# generate a random number and tell the user if they won or lost
+# generate a random number in the correct parameters and an if else statement to determine of the user won or lost the game
 secretNumber = random.randint(guessMin,guessMax)
 if (guessUser != secretNumber):
     print("You lose - the number was",secretNumber)
 else:
     print("Congratulations, you got it right!")
+
+print("Thank you for playing Guess the number.")
--- Assignment/Program 3 - Maths quest.py
+++ Assignment/Program 3 - Maths quest.py
@@ -1,20 +1,35 @@
-# inital inputs for data 
+#Author : Colin Prater
+#Date   : 11/05/2021
+#Version: 1.0
+#
+#Maths Quest
+#Teaching times tables
+
+# inital inputs for data
 name = input("Welcome to Maths Quest!  What is your name? ")
-timesTable = int (input(name+" which times table would you like to practice? (1-12)"))
 
 while True:
-    if 
-    print (name+ " on a piece of paper, write down the", timesTable ,"times table's from 1 to 12.  When you’re ready I’ll show you the answer so you can check your work.")
+    timesTable = int(input(name+" which times table would you like to practice? (1-12) "))
+    if (int(timesTable)<1 or int(timesTable)>12):
+        print ("Invalid input.")
+    else:
+        break
+
+print (name+ " on a piece of paper, write down the", timesTable ,"times table's from 1 to 12.  When you’re ready I’ll show you the answer so you can check your work.")
 
 # ready check for printing works
 while True:
-    if input("Are you Ready?") == "y":
+    if input("Are you Ready? ") == "y":
         break
-    
+
 # printing of the timestables
 for i in range(1,13):
         print(timesTable, "X" ,i, "=",timesTable * i)
 
-    
-
-
+checkforCorrect = input("Did you get them all correct? (y/n) ")
+while True:
+    if checkforCorrect == "y":
+        print("Great job! Thank you for playing Maths Quest.")
+    elif checkforCorrect == "n":
+        print ("That is unfortunate, better luck next time! Thank you for playing Maths Quest.")
+    break
