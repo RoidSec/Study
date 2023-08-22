63344c0c60deaefd00ded745b413e8b522144ac6
ChillinIX <colin.j.prater@outlook.com>
2023-08-22 15:11:06 +1000

    .

---
+++ Python/Program 6/Venndiagram.py
@@ -0,0 +1,10 @@
+from venny4py.venny4py import *
+
+sets = {
+    'set1': set(list("Harry")),
+    'set2': set(list("John")),
+    'set3': set(list("Timothy")),
+    'set4': set(list("Wilson"))
+}
+
+venny4py(sets=sets)
